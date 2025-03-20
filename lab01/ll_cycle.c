26cfe8dc2d5fafe0fd520b516ae43c80312b40b3
pandmonkey <1623972558@qq.com>
2025-03-20 21:30:58 +0800

    Project1OK

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,25 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node * tortoise = head;
+    node * hare = head;
+    while (!hare) {
+        if (hare == tortoise) {
+            return 1;
+        } 
+        hare = hare->next;
+        if (!hare) {
+            return 0;
+        }
+        if (hare == tortoise) {
+            return 1;
+        }
+        hare = hare->next;
+        tortoise = tortoise ->next;
+    }
     return 0;
+}
+
+int main() {
+    
 }
